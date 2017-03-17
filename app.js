8be8e8f11db12321ca51f599a1704f536d9985cb
Devon Moubry <devon@moubry.com>
2017-03-17 14:56:45 -0500

    format data.js style list-view add jQuery in app.js to populate list-view

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-function renderHome() {
+console.log(jQuery);
 
-}
+//function renderHome() {
+//album title and img "link" to album view
+$("li#album1 img").attr("src",albums[0]['photos'][0]['url']);
+$("li#album2 img").attr("src",albums[1]['photos'][0]['url']);
+$("li#album3 img").attr("src",albums[2]['photos'][0]['url']);
+$("li#album4 img").attr("src",albums[3]['photos'][0]['url']);
+$("li#album5 img").attr("src",albums[4]['photos'][0]['url']);
+$("li#album6 img").attr("src",albums[5]['photos'][0]['url']);
+//var $album1 = $('<li>Album 1</li>');
+//}
 
 function renderAlbum() {
-
+//h2 album title
+//nav album1, album2, ...
+//show album# selected in nav
+//display photo thumbnails "links" to photo view
 }
 
 function renderImage() {
 
 }
+
+//var $albumNav = $('.album-nav');
+//$albumNav.on('click', function (e))
+
+//$('li.album1').add('<img id="'data.name.photos.id'" src="'data.name.photos.url'" />');
+//$('li.album1').add('img', 'images/nasa-43567.jpg');
+//$('li.album1').text('img', 'data.name.photos.url');
+console.dir(albums);
+
+//for Each
+/*var $albums = $('li.outerText');
+$albums.forEach(
+
+)*/
--- data.js
+++ data.js
@@ -1,92 +1,111 @@
-var data = [
+var albums = [
   {
-    url: "images/nasa-43563.jpg",
-    id: "photo1",
-    album: "album1"
+    name: 'album1',
+    photos: [{
+       url: "images/nasa-43563.jpg",
+       id: "photo1",
+       album: "album1"
+     },
+     {
+       url: "images/nasa-43567.jpg",
+       id: "photo2",
+       album: "album1"
+     },
+     {
+       url: "images/nasa-43979.jpg",
+       id: "photo3",
+       album: "album1"
+     }]
+   },
+   {
+     name: 'album2',
+     photos: [{
+       url: "images/nasa-45067.jpg",
+       id: "photo4",
+       album: "album2"
+     },
+     {
+       url: "images/nasa-45068.jpg",
+       id: "photo5",
+       album: "album2"
+     },
+     {
+       url: "images/nasa-45069.jpg",
+       id: "photo6",
+       album: "album2"
+     }]
+   },
+  {
+    name: 'album3',
+    photos: [{
+      url: "images/nasa-45076.jpg",
+      id: "photo7",
+      album: "album3"
+    },
+    {
+      url: "images/nasa-53881.jpg",
+      id: "photo8",
+      album: "album3"
+    },
+    {
+      url: "images/nasa-53883.jpg",
+      id: "photo9",
+      album: "album3"
+    }]
   },
   {
-    url: "images/nasa-43567.jpg",
-    id: "photo2",
-    album: "album1"
+    name: 'album4',
+    photos: [{
+      url: "images/nasa-53884.jpg",
+      id: "photo10",
+      album: "album4"
+    },
+    {
+      url: "images/nasa-53888.jpg",
+      id: "photo11",
+      album: "album4"
+    },
+    {
+      url: "images/nasa-63029.jpg",
+      id: "photo12",
+      album: "album4"
+    }]
   },
   {
-    url: "images/nasa-43979.jpg",
-    id: "photo3",
-    album: "album1"
+    name: 'album5',
+    photos: [{
+      url: "images/nasa-63030.jpg",
+      id: "photo13",
+      album: "album5"
+    },
+    {
+      url: "images/nasa-63033.jpg",
+      id: "photo14",
+      album: "album5"
+    },
+    {
+      url: "images/nasa-89116.jpg",
+      id: "photo15",
+      album: "album5"
+    }]
   },
   {
-    url: "images/nasa-45067.jpg",
-    id: "photo4",
-    album: "album2"
-  },
-  {
-    url: "images/nasa-45068.jpg",
-    id: "photo5",
-    album: "album2"
-  },
-  {
-    url: "images/nasa-45069.jpg",
-    id: "photo6",
-    album: "album2"
-  },
-  {
-    url: "images/nasa-45076.jpg",
-    id: "photo7",
-    album: "album3"
-  },
-  {
-    url: "images/nasa-53881.jpg",
-    id: "photo8",
-    album: "album3"
-  },
-  {
-    url: "images/nasa-53883.jpg",
-    id: "photo9",
-    album: "album3"
-  },
-  {
-    url: "images/nasa-53884.jpg",
-    id: "photo10",
-    album: "album4"
-  },
-  {
-    url: "images/nasa-53888.jpg",
-    id: "photo11",
-    album: "album4"
-  },
-  {
-    url: "images/nasa-63029.jpg",
-    id: "photo12",
-    album: "album4"
-  },
-  {
-    url: "images/nasa-63030.jpg",
-    id: "photo13",
-    album: "album5"
-  },
-  {
-    url: "images/nasa-63033.jpg",
-    id: "photo14",
-    album: "album5"
-  },
-  {
-    url: "images/nasa-89116.jpg",
-    id: "photo15",
-    album: "album5"
-  },
-  {
-    url: "images/nasa-89125.jpg",
-    id: "photo16",
-    album: "album6"
-  },
-  {
-    url: "images/nasa-89127.jpg",
-    id: "photo17",
-    album: "album6"
-  },
-  {
-    url: "images/nasa-90395.jpg",
-    id: "photo18",
-    album: "album6"
+    name: 'album6',
+    photos:
+    [{
+      url: "images/nasa-89125.jpg",
+      id: "photo16",
+      album: "album6"
+    },
+    {
+      url: "images/nasa-89127.jpg",
+      id: "photo17",
+      album: "album6"
+    },
+    {
+      url: "images/nasa-90395.jpg",
+      id: "photo18",
+      album: "album6"
+    }]
   }
 ]
