932215ce68ec3354ad577d465edf91a6ff78539b
Devon Moubry <devon@moubry.com>
2017-03-19 15:00:08 -0500

    album photo container generated, click listener added to nav anchor to populate album pages, album view styles added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function renderHome() {
+  console.log('I rendered the home');
   //home-view container
   $('body').append('<div class="home-view"></div>');
   //heading My Albums
@@ -12,50 +13,15 @@ function renderHome() {
   $('.home-view a').on('click', renderAlbum);
 }
 
-// albums.forEach(function( value, index, array){
-//   $('.album-contents').prepend('<img src="" alt="">');
-//   $('.album-contents').attr('alt', albums[index]['name'])
-//   $('.album-contents img').attr('src', albums[index]['photos'][0]['url']);
-// }, this);
-//
-// <div class="photo-view">
-//
-//   <nav class="photo-nav">
-//
-//   </nav>
-//
-//   <h3 id="photo-title"></h3>
-//
-//   <div class="photo-contents">
-//
-//   </div>
-//
-// </div>
-
-//nav bar
-// albums.forEach(function(value, key, array, argument) {
-//     $('nav.album-nav').append('<a id="albumNav' + key + '" href="#"></a>');
-//     $('nav.album-nav a#albumNav' + key).text(albums[key]['name']);
-//   });
-//
-// //album view need to fix syntax
-// albums.forEach(function(value, key, array, argument) {
-//   $('.album-contents').append('<img id="img' + key + '" src="" alt="">');
-//   $('.album-contents img#img' + key).attr('src', albums['photos']['url']); //why doesn't this work?
-//   //$('.album-contents img').add(albums[key]['photos']['id']);
-//   });
-//
 function renderAlbum(clickEvent) {
   $('.home-view').remove();
+  $('.album-view').remove();
   console.log('I rendered the album-view');
   // console.log(clickEvent);
-
   var selectedAlbum = $.grep(albums, function(album, index) {
     return album['id'] == clickEvent.target.id
   })[0];
-
   console.log(selectedAlbum);
-
   //album-view container
   $('body').append('<div class="album-view"></div>');
   //insert album title
@@ -64,27 +30,22 @@ function renderAlbum(clickEvent) {
   $('.album-view').append('<nav class="album-nav"></nav>');
   //unordered list
   $('.album-nav').append('<ul></ul>');
-  //loop to build photos in album
+  //loop to build links in album-nav
   albums.forEach(function(element, index, array){
-    $('ul').append('<li id=""><a href="#">' + element['name']+ '</a></li>');
+    $('ul').append('<li id=""><a href="#" id="' +  element['id'] + '">' + element['name']+ '</a></li>');
   });
-  $('.album-nav a').on('click', renderImage);
-
-// //show album# selected in nav
-// //display photo thumbnails "links" to photo view
-// //forEach name generate a div with class-name album
-
-//   <div class="album-contents">
-//
-//   </div>
-//
-//
+  //adding listener to album nav links
+  $('.album-nav a').on('click', renderAlbum);
+  //album-contents container
+  $('.album-view').append('<div class="album-contents"></div>');
+  //insert unordered list into container
+  $('.album-contents').append('<ul></ul>');
+  //insert photos into album container
+  selectedAlbum['photos'].forEach(function(element, index, array){
+    $('.album-contents').append('<li><a id="' + element['id'] + '" href="#"><img src="' + element['url'] + '" alt="">' + element['name']+ '</a></li>');
+  });
 }
-// //for each album page 1,2,3,4,5,6
-// //i need to create a left side nav bar album 1, album 2, album 3, album 4, album 5, album 6
-// //title
-// //populate 3 images with caption img photo 1, img photo 2, img photo 3
-//
+
 function renderImage(eventClick) {
   console.log('I rendered the image!');
 }
